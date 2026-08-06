c262ec677272f7f6cbc826411be5dc1ad9dc39b6
kkCrops <kliang2006@gmail.com>
2026-08-05 22:02:55 -0400

    final turn in

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,10 +1,24 @@
+def test_edit_user(self):
+        new_user = self.get_user()
+        new_user.account_id = "test_id"
+        original_account_id = "0cf25a005473"
 
-        if parent_cur is None and parent_conn is None:
-            cur.close()
-            conn.commit()
-            conn.close()
+        self.db.edit_user(original_account_id=original_account_id, new_user=new_user)
 
-        else:
-            parent_conn.commit()
+        self.db.cur.execute("SELECT account_id, name, address, phone_number, email "
+                         "FROM User WHERE account_id = %s", (new_user.account_id,))
 
-    # Some SQL error, could be bad login or something else
+        edited_user = self.db.cur.fetchone()
+
+        self.db.cur.execute("SELECT account_id, name, address, phone_number, email "
+                         "FROM User WHERE account_id = %s", (original_account_id,))
+
+        old_user = self.db.cur.fetchone()
+
+        self.assertIsNone(old_user)
+
+        self.assertEqual(new_user.account_id, edited_user[0])
+        self.assertEqual(new_user.name, edited_user[1])
+        self.assertEqual(new_user.address, edited_user[2])
+        self.assertEqual(new_user.phone_number, edited_user[3])
+        self.assertEqual(new_user.email, edited_user[4])
